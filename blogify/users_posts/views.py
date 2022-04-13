a211ed52d46817292461863d72f38afb9c00d372
Mubin Ansari <mubinansari910@gmail.com>
2022-04-14 05:18:48 +0530

    add account and username views

--- blogify/users_posts/views.py
+++ blogify/users_posts/views.py
@@ -42,3 +42,19 @@ def login():
 def logout():
   logout_user()
   return redirect(url_for('core.index'))
+
+@users_posts.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+  username = current_user.username
+  email = current_user.email
+  display_picture = f'https://ui-avatars.com/api/?name={username[0]}'
+  return render_template('account.html', username=username, email=email, display_picture = display_picture)
+
+@users_posts.route('/<username>')
+def user_posts(username):
+  page = request.args.get('page', 1, type=int)
+  user = User.query.filter_by(username=username).first_or_404()
+  blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+  return render_template('user_blog_posts.html', blog_posts = blog_posts, user = user)
+  
