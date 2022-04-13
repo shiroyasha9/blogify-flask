70d0cb7ab4d68cb516edc499c9cb7e89c5dcc56a
Mubin Ansari <mubinansari910@gmail.com>
2022-04-14 03:39:36 +0530

    add 404 and 403 error pages

--- blogify/__init__.py
+++ blogify/__init__.py
@@ -1,7 +1,15 @@
 # blogify/__init__.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def error_404(error):
+  return render_template('error_pages/404.html'), 404
+
+@app.errorhandler(403)
+def error_403(error):
+  return render_template('error_pages/403.html'), 403
 
 from blogify.core.views import core
 app.register_blueprint(core)
