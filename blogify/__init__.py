593f6a49308e7af347eba2df10e49d9ddaec5e6b
Mubin Ansari <mubinansari910@gmail.com>
2022-04-14 03:42:46 +0530

    add models for user and blogPost

--- blogify/__init__.py
+++ blogify/__init__.py
@@ -1,7 +1,27 @@
 # blogify/__init__.py
 from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+# Database setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+# Login Configs
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+# Error handling
 
 @app.errorhandler(404)
 def error_404(error):
