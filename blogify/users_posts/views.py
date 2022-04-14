45cc2604e1b96c92266500313ad7f4e2c543f93e
Mubin Ansari <mubinansari910@gmail.com>
2022-04-14 05:32:28 +0530

    add user blog posts template

--- blogify/users_posts/views.py
+++ blogify/users_posts/views.py
@@ -56,5 +56,6 @@ def user_posts(username):
   page = request.args.get('page', 1, type=int)
   user = User.query.filter_by(username=username).first_or_404()
   blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
-  return render_template('user_blog_posts.html', blog_posts = blog_posts, user = user)
+  display_picture = f'https://ui-avatars.com/api/?name={username[0]}'
+  return render_template('user_blog_posts.html', blog_posts = blog_posts, user = user, display_picture = display_picture)
   
